fe6c5d5ca5262271c7264b4957e739f5ba6302e5
fleite174 <andretsilva2509@gmail.com>
2023-07-07 09:36:28 -0300

    ex32

---
+++ pythonExerciciosEx32.py
@@ -0,0 +1,7 @@
+## Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+
+
+distancia = float(input('Qual é a distancia da sua viagem? '))
+print('Você esta prestes a começar uma viagem de {} km.'.format(distancia))
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('E o preço da sua passagem será de R${:.2F}'.format(preco))
